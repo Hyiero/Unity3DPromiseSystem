ecde145a0edf514ef2ba5eb9eb0a63ae0c93b3e8
Hyiero <hyiero89@gmail.com>
2016-09-27 00:08:03 -0400

    Working on Promise System. So far my head is spinning in circles

---
+++ Assets/Code/Promise.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class Promise<T> : IPromise<T>
+{
+    List<Action<T>> resolveCallbacks = new List<Action<T>>();
+    
+    public Promise(Action<T> resolveCallback)
+    {
+        resolveCallbacks.Add(resolveCallback);
+    }
+
+    public void Resolve(T variableToResolve)
+    {
+        foreach(var callback in resolveCallbacks)
+        {
+            callback(variableToResolve);
+        }
+    }
+
+    public void Reject()
+    {
+
+    }
+}
---
+++ Assets/Code/Testing.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Testing : MonoBehaviour 
+{
+    Hero horseHero;
+    void Awake()
+    {
+        horseHero = new Hero();
+        //status is what we will pass into the method when we invoke it
+        //StartCoroutine(DoMoreStuff(success => Debug.Log(success), error => Debug.Log(error)));
+        PromiseStuff();
+    }
+
+    void Update()
+    {
+        //Debug.Log("Result: "+result);
+    }
+    public IEnumerator DoMoreStuff(Action<string> resolve)
+    {
+        var success = true;
+        yield return new WaitForSeconds(2);
+
+        if (success)
+        {
+            resolve("Success");
+        }
+    }
+
+    public IPromise<string> PromiseStuff()
+    {
+        var promise = new Promise<string>(result => Debug.Log(result));
+        StartCoroutine(DoMoreStuff(resolved => promise.Resolve(resolved)));
+
+        return promise;
+    }
+
+    public void Load(string status,Func<string,string> resolve,Func<string,string> reject)
+    {
+        if (status == "Success")
+            resolve("We Succeed!");
+        else
+            reject("We Failed Bad!");
+    }
+}
+
+public class Hero
+{
+    public int level = 1;
+    public void LevelUp()
+    {
+        level++;
+    }
+}
